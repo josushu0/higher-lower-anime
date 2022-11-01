d6e0e843f484c7b167acc10e5735484de219a8d8
Josué Martell <josue.martell17@gmail.com>
2022-11-01 15:52:21 -0600

    feat: added my eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'@josushu0'
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': ['react']
 }
