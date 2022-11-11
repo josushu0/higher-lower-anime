59c9d61c678205f7d3c5cd737588820a1a4a27e5
Josué Martell <josue.martell17@gmail.com>
2022-11-10 22:45:04 -0600

    feat: display score and max score in lose screen

--- src/components/Lose.jsx
+++ src/components/Lose.jsx
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types'
 import styles from './Lose.module.css'
 
-function Lose({ play, back }) {
+function Lose({ play, back, score }) {
+	if (localStorage.getItem('highScore') < score) {
+		localStorage.setItem('highScore', score)
+	}
+
 	function goHome() {
 		back('home')
 	}
@@ -13,6 +17,10 @@ function Lose({ play, back }) {
 	return (
 		<main className={styles.lose}>
 			<h1 className={styles.title}>Game Over :(</h1>
+			<div className={styles.scores}>
+				<span className={styles.score}>Score: {score}</span>
+				<span className={styles.score}>Max Score: {localStorage.getItem('highScore')}</span>
+			</div>
 			<button className={styles.play} onClick={playAgain}>Try again</button>
 			<button className={styles.back} onClick={goHome}>Return Home</button>
 		</main>
@@ -21,7 +29,8 @@ function Lose({ play, back }) {
 
 Lose.propTypes = {
 	play: PropTypes.func,
-	back: PropTypes.func
+	back: PropTypes.func,
+	score: PropTypes.number
 }
 
 export default Lose
