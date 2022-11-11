f67e41d18fe7a7d7982125bcaa5f1d9c87e5749b
Josué Martell <josue.martell17@gmail.com>
2022-11-10 21:16:02 -0600

    feat: handle game logic

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,16 +1,54 @@
+import { ChevronDoubleLeftIcon } from '@heroicons/react/24/solid'
+import PropTypes from 'prop-types'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import styles from './Game.module.css'
 import Tile from './Tile'
 
-function Game() {
+function Game({ anime, back, lose }) {
+	const [index, setIndex] = useState(0)
+	const [left, setLeft] = useState(anime[ index ])
+	const [right, setRight] = useState(anime[ index + 1 ])
+
+	useEffect(() => {
+		setLeft(anime[ index ])
+		setRight(anime[ index + 1 ])
+	}, [index])
+
+	function handleNext(option) {
+		if (option === 'higher') {
+			if (left.rank > right.rank) {
+				setIndex(index + 1)
+			} else {
+				lose('lose')
+			}
+		} else {
+			if (left.rank > right.rank) {
+				lose('lose')
+			} else {
+				setIndex(index + 1)
+			}
+		}
+	}
+
 	return (
 		<main className={styles.game}>
-			<Tile left={true} />
-			<Tile left={false} />
+			<Tile left={true} anime={left} />
+			<Tile left={false} anime={right} next={handleNext} />
+			<button className={styles.back} onClick={() => back('home')}>
+				<ChevronDoubleLeftIcon className={styles.backArrow} />
+			</button>
 			<div className={styles.versus_circle}>
 				<span className={styles.versus_text}>VS</span>
 			</div>
 		</main>
 	)
+}
+
+Game.propTypes = {
+	anime: PropTypes.array,
+	back: PropTypes.func,
+	lose: PropTypes.func
 }
 
 export default Game
